9b47ae65cc3a0529a344bc5e2caf4feebecc0540
Nathan Dykman <nathan.dykman@act-on.net>
2015-08-20 13:29:22 -0700

    Adding needed files to branch before merge back to master

---
+++ Microsoft.Research/common/include/Version.cs
@@ -0,0 +1,9 @@
+// Warning: Automatically generated file. DO NOT EDIT
+// Generated at Thursday, August 20, 2015 1:06:18 PM
+
+using System.Reflection;
+[assembly: AssemblyVersion("1.9.10820.1")]
+[assembly: AssemblyFileVersion("1.9.10820.1")]
+[assembly: AssemblyCompany("Microsoft Corporation")]
+[assembly: AssemblyCopyright("(c) Microsoft Corporation.  All rights reserved.")]
+
